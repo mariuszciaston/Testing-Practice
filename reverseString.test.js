e540bc2b864d65caced0ae226ada77c9a27c2143
Mariusz <casmar90@gmail.com>
2023-12-11 16:57:11 +0100

    Move to testing folder

--- reverseString.test.js
+++
@@ -1,7 +0,0 @@
-import reverseString from './reverseString';
-
-test('should output reversed string', () => {
-	expect(reverseString('abcd')).toBe('dcba');
-	expect(reverseString('1234')).toBe('4321');
-	expect(reverseString('QWERTYUIOP')).toBe('POIUYTREWQ');
-});
